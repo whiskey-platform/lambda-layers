ace13bd02ad00b8ff4df50e3e909ea565f71328a
Matthew Wyskiel <matt@mattwyskiel.com>
2023-08-20 00:33:07 -0400

    move yt-dlp download code into deployment yaml

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -3,13 +3,8 @@ import { join } from 'path';
 import { StackContext } from 'sst/constructs';
 import { request as github } from '@octokit/request';
 import axios from 'axios';
-import { createReadStream, createWriteStream } from 'fs';
-import { finished, pipeline } from 'stream/promises';
-import AdmZip from 'adm-zip';
-import { execSync } from 'child_process';
-import * as tar from 'tar';
-import { copyFile } from 'fs/promises';
-import { createDecompressor } from 'lzma-native';
+import { createWriteStream } from 'fs';
+import { finished } from 'stream/promises';
 
 async function getLatestChromium() {
   console.log('Fetching latest release of chromium');
@@ -42,24 +37,6 @@ async function getLatestYTDLP() {
     repo: 'yt-dlp',
   });
   const latestRelease = releases.data[0];
-  console.log('Latest release of yt-dlp is ' + latestRelease.name);
-  const latestReleaseZip = latestRelease.assets.filter(v => v.name === `yt-dlp_linux`)[0];
-  console.log('downloading binary');
-  await downloadFile(
-    latestReleaseZip.browser_download_url,
-    join('layers', 'yt-dlp', 'bin', latestReleaseZip.name)
-  );
-  console.log(
-    'successfully downloaded binary to ' + join('layers', 'yt-dlp', 'bin', latestReleaseZip.name)
-  );
-  console.log('downloading tar.xz of ffmpeg');
-  downloadFFMPEG();
-
-  installYTDLPNode();
-
-  const zip = new AdmZip();
-  zip.addLocalFolder(join('layers', 'yt-dlp', 'bin'));
-  await zip.writeZipPromise('layers/yt-dlp/bin.zip');
   return { latestRelease };
 }
 const ytDLPZip = await getLatestYTDLP();
@@ -85,20 +62,6 @@ export function Layers({ stack }: StackContext) {
 //   execSync(`cd ${join(__dirname, path)}`);
 //   execSync('pnpm i');
 // }
-
-function downloadFFMPEG() {
-  execSync(
-    'cd layers/yt-dlp/build && curl https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar x'
-  );
-  execSync(
-    'mv layers/yt-dlp/build/ffmpeg*/ffmpeg layers/yt-dlp/build/ffmpeg*/ffprobe layers/yt-dlp/bin'
-  );
-}
-
-function installYTDLPNode() {
-  execSync(`cd ${join('layers', 'yt-dlp')}`);
-  execSync('YOUTUBE_DL_SKIP_DOWNLOAD=true pnpm i');
-}
 
 // https://stackoverflow.com/questions/55374755/node-js-axios-download-file-stream-and-writefile
 export async function downloadFile(fileUrl: string, outputLocationPath: string): Promise<any> {
